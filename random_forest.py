212f786a919a0fafb151a2099abfd96b58f906a1
Matty Jones <mattyjones@boisestate.edu>
2018-06-18 11:53:00 -0600

    Different columns can be skipped from training using -s and different features to fit to by -y in random_forest.py. Fixes #3.

--- random_forest.py
+++ random_forest.py
@@ -17,7 +17,7 @@ from sklearn.externals import joblib
 
 
 def get_data(database="p3ht.db", training_tables=None, validation_tables=None,
-             absolute=None):
+             absolute=None, skip=None, yval="TI"):
     training_records = []
     print("".join(["Loading data from ", database, "..."]))
     # Obtain training tables first:
@@ -32,11 +32,15 @@ def get_data(database="p3ht.db", training_tables=None, validation_tables=None,
         cursor.close()
         connection.close()
     for table_name in training_tables:
-        data = load_table(database, table_name)
+        data, all_column_names = load_table(database, table_name)
         for record in data:
             training_records.append(record)
+    column_names_to_use = list(set(all_column_names) - set(skip))
     train_features, train_labels = create_data_frames(np.array(training_records),
-                                                      absolute)
+                                                      absolute,
+                                                      all_column_names,
+                                                      column_names_to_use,
+                                                      yval)
     print("Separating training and test data...")
     if validation_tables is None:
         # Split the dataset we have to be 95%:5%
@@ -45,56 +49,26 @@ def get_data(database="p3ht.db", training_tables=None, validation_tables=None,
         # Need to load the right validation data:
         validation_records = []
         for table_name in validation_tables:
-            data = load_table(database, table_name)
+            data, _ = load_table(database, table_name)
             for record in data:
                 validation_records.append(record)
-            test_features, test_labels = create_data_frames(np.array(validation_records),
-                                                            absolute)
+        test_features, test_labels = create_data_frames(np.array(validation_records),
+                                                        absolute,
+                                                        column_names_to_use,
+                                                        yval)
         return train_features, test_features, train_labels, test_labels
 
 
-def create_data_frames(data, absolute):
-    df = pd.DataFrame(
-        data,
-        columns=[
-            "Chromophore1",
-            "Chromophore2",
-            "posX",
-            "posY",
-            "posZ",
-            "rotX",
-            "rotY",
-            "rotZ",
-            "DeltaE",
-            "same_chain",
-            "sulfur_dist",
-            "TI",
-        ],
-    )
-
-    df = df.drop(["Chromophore1", "Chromophore2"], axis=1)
+def create_data_frames(data, absolute, all_column_names, column_names_to_use,
+                       yval):
+    df = pd.DataFrame(data, columns=all_column_names)
 
     if absolute is not None:
         for col_name in absolute:
             df[col_name] = df[col_name].abs()
-
-    features = df[
-        [
-            "posX",
-            "posY",
-            "posZ",
-            "rotX",
-            "rotY",
-            "rotZ",
-            "DeltaE",
-            "same_chain",
-            "sulfur_dist",
-        ]
-    ]
-
-    y = df[["TI"]]
+    features = df[column_names_to_use]
+    y = df[[yval]]
     return features, y
-
 
 
 def load_table(database, table):
@@ -105,9 +79,11 @@ def load_table(database, table):
     cursor.execute(query)
     data = cursor.fetchall()
     data = np.array(data)
+    cursor.execute("PRAGMA table_info({})".format(table))
+    column_names = [col_props[1] for col_props in cursor.fetchall()]
     cursor.close()
     connection.close()
-    return np.array(data)
+    return np.array(data), column_names
 
 
 def plot_tree(reg):
@@ -368,6 +344,27 @@ if __name__ == "__main__":
                         descriptors. E.g. -a rotX rotY""",
     )
     parser.add_argument(
+        "-s",
+        "--skip",
+        nargs='+',
+        type=str,
+        default=[],
+        required=False,
+        help="""When column names are passed, random_forest.py
+                        will skip over these descriptors, meaning
+                        that the ML will not train on them.
+                        E.g. -s DeltaE""",
+    )
+    parser.add_argument(
+        "-y",
+        "--yval",
+        type=str,
+        default="TI",
+        required=False,
+        help="""When specified, use a feature other than "TI"
+                        to train on and fit to.""",
+    )
+    parser.add_argument(
         '-t',
         '--training',
         nargs='+',
@@ -390,6 +387,16 @@ if __name__ == "__main__":
                         T1_5 T1_75 T2_0""",
     )
     args = parser.parse_args()
+
+    # Don't want to train on the chromophore_IDs!
+    chromophore_ID_cols = ["chromophoreA", "chromophoreB"]
+    for chromophore_ID_col in chromophore_ID_cols:
+        if chromophore_ID_col not in args.skip:
+            args.skip.append(chromophore_ID_col)
+    # Also don't want to train on the answer!
+    if args.yval not in args.skip:
+        args.skip.append(args.yval)
+
     # compare_four()
 
     train_features, test_features, train_labels, test_labels = get_data(
@@ -397,6 +404,8 @@ if __name__ == "__main__":
         training_tables=args.training,
         validation_tables=args.validation,
         absolute=args.absolute,
+        skip=args.skip,
+        yval=args.yval,
     )
 
     # Concatenate the entire dataset
