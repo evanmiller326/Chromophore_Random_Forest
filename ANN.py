5a7057897e6d66ef3d23148f059f2f351bd9e86d
EvanBSU <evanmiller326@boisestate.edu>
2018-11-30 16:16:40 -0700

    Updating ANN to use the helper functions.

--- ANN.py
+++ ANN.py
@@ -1,74 +1,10 @@
 import tensorflow as tf
 import numpy as np
-import sqlite3
 
-import sklearn
-from sklearn.preprocessing import normalize
-from sklearn import preprocessing
-from scipy import stats
-
-import matplotlib.pyplot as plt
-import ml_helpers as mlh
-
-def convolve(x, n_filters, outsize):
-    x = tf.reshape(x, [-1, 1, x.shape[1], 1])
-    w_conv = tf.Variable(tf.random_normal([1, 1, 1, n_filters]))
-    b_conv = tf.Variable(tf.random_normal([n_filters]))
-    conv = tf.nn.convolution(input = x, filter = w_conv, padding = "VALID", strides = [1, 1])
-    conv = tf.nn.relu(tf.add(conv, b_conv))
-    pool = tf.nn.pool(
-            input=conv,
-            window_shape= [1, 1],
-            pooling_type = "MAX",
-            padding = "SAME",
-            strides = [1,1])
-    data_len = pool.shape[2]
-    filter_depth = pool.shape[3]
-    size = int(data_len*filter_depth)
-
-    pool = tf.reshape(pool, [-1, size])
-    pool = tf.convert_to_tensor(pool, dtype=tf.float32)
-
-    W = tf.random_normal(shape=[size, outsize], mean=0.2, stddev=0.2)
-    b = tf.random_normal(shape=[outsize], mean = 0.2, stddev = 0.2)
-
-    xconv = tf.nn.relu(tf.add(tf.matmul(pool, W), b))
- #   xconv = tf.nn.tanh(tf.add(tf.matmul(pool, W), b))
-
-    return xconv
-
-def weights(input_vector, in_nodes, out_nodes):
-    W = tf.random_normal(shape=[in_nodes, out_nodes], mean=0.2, stddev=0.2)
-    b = tf.random_normal(shape=[out_nodes], mean = 0.2, stddev = 0.2)
-    #W = tf.random_normal(shape=[in_nodes, out_nodes], stddev=0.1)
-    #b = tf.random_normal(shape=[out_nodes], stddev = 0.1)
-    return build_layer(input_vector, tf.Variable(W), tf.Variable(b))
-
-def get_batch(vectors, labels):
-    indices = np.arange(len(vectors))
-    np.random.shuffle(indices)
-    chosen = indices[:1000]
-    return vectors[chosen], labels[chosen]
-
-def build_layer(x, W, b):
-    return tf.add(tf.matmul(x,W),b)
-
-def plot_comparison(actual, predicted, rmse, rsquared):
-    plt.scatter(actual, predicted, zorder=0, alpha = 0.5, s=12)
-    plt.plot(np.linspace(0, np.amax(actual), 10), np.linspace(0, np.amax(actual), 10), c='k', zorder = 10, label="MAE={:.3f},\nR$^2$={:.2f}".format(rmse, rsquared))
-    #plt.title("ANN-RMSE-{:.5f}".format(rmse))
-    plt.xticks([0.0, 0.5, 1.0, 1.5])
-    plt.yticks([0.0, 0.5, 1.0, 1.5])
-    plt.xlabel("Actual (eV)")
-    plt.ylabel("Predicted (eV)")
-    plt.legend(loc = 'upper left')
-    plt.savefig("Ann_comp.png")
-
-#Commented out to test if function is unused.
-#def parallel_sort(a, b):
-#    temp_a = np.copy(np.array(a))
-#    temp_b = np.copy(np.array(b))
-#    return zip(*sorted(zip(temp_a, temp_b)))
+from helper_functions import nn_helpers as nnh
+from helper_functions import load_data_helper as ldh
+from helper_functions import general_helpers as gh
+from helper_functions import plotter_helper as ph
 
 def find_largest_deviations(chromo_IDs, pred_y, actual_y):
     differences = np.array(actual_y) - np.array(pred_y)
@@ -93,14 +29,6 @@ def find_largest_deviations(chromo_IDs, pred_y, actual_y):
             break
     return error_dictionary
 
-def plot_error_hist(error_dictionary):
-    plt.figure()
-    plt.hist(list(error_dictionary.keys()), bins=100)
-    plt.xlabel("Error")
-    plt.ylabel("Frequency")
-    plt.xlim([-0.5, 0.5])
-    plt.savefig("./error_histogram_all.png")
-
 def run_net(database,
         training,
         validation,
@@ -124,7 +52,7 @@ def run_net(database,
     if yval not in skip:
         skip.append(yval)
 
-    train_features, test_features, train_labels, test_labels = mlh.get_data(
+    train_features, test_features, train_labels, test_labels = ldh.get_data(
         database=database,
         training_tables=training,
         validation_tables=validation,
@@ -142,15 +70,15 @@ def run_net(database,
 
     layer_dict = {}
 
-    xconv = convolve(x, nfilters, convolution_outsize)
+    xconv = nnh.convolve(x, nfilters, convolution_outsize)
 
     for N in range(Nlayers):
         if N == 0:
-            layer_dict["layer_{}".format(N)] = tf.nn.elu(weights(x, convolution_outsize, N_nodes[N]))
+            layer_dict["layer_{}".format(N)] = tf.nn.elu(nnh.weights(x, convolution_outsize, N_nodes[N]))
         elif N + 1 == Nlayers:
-            layer_dict["layer_{}".format(N)] = tf.nn.elu(weights(layer_dict["layer_{}".format(N-1)], N_nodes[N-1], N_nodes[N]))
+            layer_dict["layer_{}".format(N)] = tf.nn.elu(nnh.weights(layer_dict["layer_{}".format(N-1)], N_nodes[N-1], N_nodes[N]))
         else:
-            layer_dict["layer_{}".format(N)] = tf.nn.elu(weights(layer_dict["layer_{}".format(N-1)], N_nodes[N-1], N_nodes[N]))
+            layer_dict["layer_{}".format(N)] = tf.nn.elu(nnh.weights(layer_dict["layer_{}".format(N-1)], N_nodes[N-1], N_nodes[N]))
 
     y_out = layer_dict["layer_{}".format(Nlayers-1)]
 
@@ -170,7 +98,7 @@ def run_net(database,
     training_error = []
 
     for _ in range(int(training_iterations)):
-        batch_vectors, batch_answers = get_batch(train_features, train_labels)
+        batch_vectors, batch_answers = nnh.get_batch(train_features, train_labels)
         session.run(training_step,feed_dict={x:batch_vectors,y_:batch_answers})
         if _ % (int(training_iterations)//10) == 0:
 
@@ -180,19 +108,13 @@ def run_net(database,
 
     pred_y = session.run(y_out, feed_dict={x: test_features})
 
-    #rmse = session.run(cost,feed_dict={x:test_features,y_:test_labels})
-    mae = np.mean(abs(pred_y-test_labels))
-
-    slope, intercept, r_value, p_value, std_err = stats.linregress(
-        test_labels.flatten(), pred_y.flatten()
-    )
+    r_value, mae = gh.calc_comparison(test_labels, pred_y)
 
     #error_dictionary = find_largest_deviations(chromo_IDs, pred_y, test_labels)
 
-    #plot_error_hist(error_dictionary)
-    plot_comparison(test_labels, abs(pred_y), mae, r_value**2)
+    ph.plot_based_on_density(test_labels.flatten(), pred_y.flatten(), "ANN", r_value**2, mae, save=True)
 
-    saver.save(session, "./p3ht_brain")
+    #saver.save(session, "./p3ht_brain")
 
 def brain(database="p3ht.db", 
         absolute=None, 
@@ -202,7 +124,7 @@ def brain(database="p3ht.db",
         validation=None,
         Nlayers = 3,
         node_comb = [10, 5, 1],
-        steps = 2e4,
+        steps = 1e4,
         run_name = "",
         forward_hops_only = False,
         show_comparison = False, 
